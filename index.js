2adb443ba3ecb57726281fba3996424c5c312850
Luke0195 <lucas201273@gmail.com>
2020-09-21 09:26:36 -0300

    adicionando arquivos estaticos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs'); // Estamos informando ao express que o ejs será responsável pelas views.
+app.use(express.static('public')) // Isso vai me permitir trabalhar com arquivos estaticos no express(os arquivos estaticos são arquivos que o back-end não ira processar);
 
 app.get('/:nome/:lang', (request, response)=>{
     const { nome , lang} = request.params;
