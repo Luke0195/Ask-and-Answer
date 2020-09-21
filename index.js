5d4019b4f0146e6cbec273b3196bb2ea5f134bc0
Luke0195 <lucas201273@gmail.com>
2020-09-21 08:19:04 -0300

    add views and configuration of ejs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs'); // Estamos informando ao express que o ejs será responsável pelas views.
+
 app.get('/', (request, response)=>{
-  return response.json({message: 'Teste2'});
+  return response.render('index');
 });
 
 app.listen(3333, ()=>{
-  console.log('Server is runing');
+  console.log('App rodando ');
   
 })
