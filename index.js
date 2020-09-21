c69ce33a7974591321d1615de744f98eb6a74c2f
Luke0195 <lucas201273@gmail.com>
2020-09-21 09:17:26 -0300

    configurando estrutura de repetiçao

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ app.set('view engine', 'ejs'); // Estamos informando ao express que o ejs será 
 app.get('/:nome/:lang', (request, response)=>{
     const { nome , lang} = request.params;
     const exibirMsg = false;
+    const produtos =[
+      {nome: 'doritos', preco:4.20},
+      {nome: 'coca-cola', preco: 8.00},
+      {nome:  'play5', preco:5.000}
+
+    ]
+
   return response.render('index', {
     nome, lang, 
-    msg: exibirMsg
+    msg: exibirMsg,
+    produtos
   });
 });
 
