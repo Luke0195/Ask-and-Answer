89e7da844a68accde081409dbf8f75aa4aa17fa3
Luke0195 <lucas201273@gmail.com>
2020-09-21 13:49:29 -0300

    feat: capturando as informaçoes do formulario

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');  // é uma extensão que nós permite capturar as informações do formulario para javascript
 
-app.set('view engine', 'ejs'); // Estamos informando ao express que o ejs será responsável pelas views.
-app.use(express.static('public')) // Isso vai me permitir trabalhar com arquivos estaticos no express(os arquivos estaticos são arquivos que o back-end não ira processar);
+
+app.set('view engine', 'ejs'); // Estamos informando ao express que o ejs será responsável pelas views.n
+app.use(express.static('public')); // Isso vai me permitir trabalhar com arquivos estaticos no express(os arquivos estaticos são arquivos que o back-end não ira processar);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/', (request, response)=>{
   return response.render('index');
 });
 
-app.get('/askQuestion', (request,response)=>{
+app.get('/askquestion', (request,response)=>{
+ 
+  console.log(obj);
   return response.render('askQuestion');
-})
+});
+
+app.post('/savequestion', (request,response)=>{
+  const titulo = request.body.titulo;
+  const descricao = request.body.descricao;
+  const obj  ={
+    titulo,
+    descricao
+  }
+  console.log(obj);
+  
+});
 
 app.listen(3333, ()=>{
   console.log('App rodando ');
