6bd1011857c3ffa7ef0600f0fd07b89640623b4d
Luke0195 <lucas201273@gmail.com>
2020-09-21 09:39:10 -0300

    add bootstrap

--- index.js
+++ index.js
@@ -4,21 +4,8 @@ const app = express();
 app.set('view engine', 'ejs'); // Estamos informando ao express que o ejs será responsável pelas views.
 app.use(express.static('public')) // Isso vai me permitir trabalhar com arquivos estaticos no express(os arquivos estaticos são arquivos que o back-end não ira processar);
 
-app.get('/:nome/:lang', (request, response)=>{
-    const { nome , lang} = request.params;
-    const exibirMsg = false;
-    const produtos =[
-      {nome: 'doritos', preco:4.20},
-      {nome: 'coca-cola', preco: 8.00},
-      {nome:  'play5', preco:5.000}
-
-    ]
-
-  return response.render('index', {
-    nome, lang, 
-    msg: exibirMsg,
-    produtos
-  });
+app.get('/', (request, response)=>{
+  return response.render('index');
 });
 
 app.listen(3333, ()=>{
